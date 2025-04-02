d8f6e9ea0f9dd34437fed73ef4bacd020808db3e
nicolasambram <nicolasambram@gmail.com>
2025-04-02 12:12:41 -0300

    Actualizar main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,20 +1,195 @@
 namespace SpriteKind {
     export const a = SpriteKind.create()
     export const b = SpriteKind.create()
+    export const c = SpriteKind.create()
+    export const d = SpriteKind.create()
 }
+controller.player4.onButtonEvent(ControllerButton.A, ControllerButtonEvent.Pressed, function () {
+    player4.vy = -200
+})
+controller.player3.onButtonEvent(ControllerButton.B, ControllerButtonEvent.Pressed, function () {
+    player3.setPosition(8, 54)
+})
+controller.player1.onButtonEvent(ControllerButton.Right, ControllerButtonEvent.Pressed, function () {
+    player1.setImage(img`
+        . . . . . . . . . . b 2 b . . . 
+        . . . . . . . . . b 2 b . . . . 
+        . . . . . . . . . b c . . . . . 
+        . . . . . . b b b b b b . . . . 
+        . . . . . b b 2 2 2 2 2 b . . . 
+        . . . . b b 2 d 1 f 2 2 d f . . 
+        . . . . b 2 2 1 f f 2 d 4 c . . 
+        . . . . b 2 2 d f b d d 4 4 . . 
+        b d d d b b d 2 2 2 4 4 4 4 4 b 
+        b b d 2 2 2 b 2 2 4 4 4 4 4 b . 
+        b d c 2 2 2 2 d 2 2 2 2 2 b . . 
+        c d d c d 2 2 b 2 2 2 2 2 2 b . 
+        c b d d c c b 2 2 2 2 2 2 2 b . 
+        . c d d d d d d 2 2 2 2 2 d b . 
+        . . c b d d d d d 2 2 2 b b . . 
+        . . . c c c c c c c c b b . . . 
+        `)
+})
+controller.player4.onButtonEvent(ControllerButton.Left, ControllerButtonEvent.Pressed, function () {
+    player4.setImage(img`
+        . . . b 9 b . . . . . . . . . . 
+        . . . . b 9 b . . . . . . . . . 
+        . . . . . c b . . . . . . . . . 
+        . . . . b b b b b b . . . . . . 
+        . . . b 9 9 9 9 9 b b . . . . . 
+        . . f 6 9 9 f 1 6 9 b b . . . . 
+        . . c 4 6 9 f f 1 9 9 b . . . . 
+        . . 4 4 6 6 b f 6 9 9 b . . . . 
+        b 4 4 4 4 4 9 9 9 6 b b 6 6 6 b 
+        . b 4 4 4 4 4 9 9 b 9 9 9 6 b b 
+        . . b 9 9 9 9 9 6 9 9 9 9 c 6 b 
+        . b 9 9 9 9 9 9 b 9 9 6 c 6 6 c 
+        . b 9 9 9 9 9 9 9 b c c 6 6 b c 
+        . b 6 9 9 9 9 9 6 6 6 6 6 6 c . 
+        . . b b 9 9 9 6 6 6 6 6 b c . . 
+        . . . b b c c c c c c c c . . . 
+        `)
+})
 controller.player2.onButtonEvent(ControllerButton.A, ControllerButtonEvent.Pressed, function () {
     player2.vy = -200
+})
+controller.player4.onButtonEvent(ControllerButton.B, ControllerButtonEvent.Pressed, function () {
+    player4.setPosition(40, 54)
+})
+controller.player3.onButtonEvent(ControllerButton.Right, ControllerButtonEvent.Pressed, function () {
+    player3.setImage(img`
+        . . . . . . . . . . b 7 b . . . 
+        . . . . . . . . . b 7 b . . . . 
+        . . . . . . . . . b c . . . . . 
+        . . . . . . b b b b b b . . . . 
+        . . . . . b b 7 7 7 7 7 b . . . 
+        . . . . b b 7 6 1 f 7 7 6 f . . 
+        . . . . b 7 7 1 f f 7 6 4 c . . 
+        . . . . b 7 7 6 f b 6 6 4 4 . . 
+        b 6 6 6 b b 6 7 7 7 4 4 4 4 4 b 
+        b b 6 7 7 7 b 7 7 4 4 4 4 4 b . 
+        b 6 c 7 7 7 7 6 7 7 7 7 7 b . . 
+        c 6 6 c 6 7 7 b 7 7 7 7 7 7 b . 
+        c b 6 6 c c b 7 7 7 7 7 7 7 b . 
+        . c 6 6 6 6 6 6 7 7 7 7 7 6 b . 
+        . . c b 6 6 6 6 6 7 7 7 b b . . 
+        . . . c c c c c c c c b b . . . 
+        `)
+})
+controller.player2.onButtonEvent(ControllerButton.Left, ControllerButtonEvent.Pressed, function () {
+    player2.setImage(img`
+        . . . b 5 b . . . . . . . . . . 
+        . . . . b 5 b . . . . . . . . . 
+        . . . . . c b . . . . . . . . . 
+        . . . . b b b b b b . . . . . . 
+        . . . b 5 5 5 5 5 b b . . . . . 
+        . . f d 5 5 f 1 d 5 b b . . . . 
+        . . c 4 d 5 f f 1 5 5 b . . . . 
+        . . 4 4 d d b f d 5 5 b . . . . 
+        b 4 4 4 4 4 5 5 5 d b b d d d b 
+        . b 4 4 4 4 4 5 5 b 5 5 5 d b b 
+        . . b 5 5 5 5 5 d 5 5 5 5 c d b 
+        . b 5 5 5 5 5 5 b 5 5 d c d d c 
+        . b 5 5 5 5 5 5 5 b c c d d b c 
+        . b d 5 5 5 5 5 d d d d d d c . 
+        . . b b 5 5 5 d d d d d b c . . 
+        . . . b b c c c c c c c c . . . 
+        `)
 })
 controller.player1.onButtonEvent(ControllerButton.A, ControllerButtonEvent.Pressed, function () {
     player1.vy = -200
 })
+controller.player3.onButtonEvent(ControllerButton.A, ControllerButtonEvent.Pressed, function () {
+    player3.vy = -200
+})
+controller.player3.onButtonEvent(ControllerButton.Left, ControllerButtonEvent.Pressed, function () {
+    player3.setImage(img`
+        . . . b 7 b . . . . . . . . . . 
+        . . . . b 7 b . . . . . . . . . 
+        . . . . . c b . . . . . . . . . 
+        . . . . b b b b b b . . . . . . 
+        . . . b 7 7 7 7 7 b b . . . . . 
+        . . f 6 7 7 f 1 6 7 b b . . . . 
+        . . c 4 6 7 f f 1 7 7 b . . . . 
+        . . 4 4 6 6 b f 6 7 7 b . . . . 
+        b 4 4 4 4 4 7 7 7 6 b b 6 6 6 b 
+        . b 4 4 4 4 4 7 7 b 7 7 7 6 b b 
+        . . b 7 7 7 7 7 6 7 7 7 7 c 6 b 
+        . b 7 7 7 7 7 7 b 7 7 6 c 6 6 c 
+        . b 7 7 7 7 7 7 7 b c c 6 6 b c 
+        . b 6 7 7 7 7 7 6 6 6 6 6 6 c . 
+        . . b b 7 7 7 6 6 6 6 6 b c . . 
+        . . . b b c c c c c c c c . . . 
+        `)
+})
+controller.player1.onButtonEvent(ControllerButton.Left, ControllerButtonEvent.Pressed, function () {
+    player1.setImage(img`
+        . . . b 2 b . . . . . . . . . . 
+        . . . . b 2 b . . . . . . . . . 
+        . . . . . c b . . . . . . . . . 
+        . . . . b b b b b b . . . . . . 
+        . . . b 2 2 2 2 2 b b . . . . . 
+        . . f d 2 2 f 1 d 2 b b . . . . 
+        . . c 4 d 2 f f 1 2 2 b . . . . 
+        . . 4 4 d d b f d 2 2 b . . . . 
+        b 4 4 4 4 4 2 2 2 d b b d d d b 
+        . b 4 4 4 4 4 2 2 b 2 2 2 d b b 
+        . . b 2 2 2 2 2 d 2 2 2 2 c d b 
+        . b 2 2 2 2 2 2 b 2 2 d c d d c 
+        . b 2 2 2 2 2 2 2 b c c d d b c 
+        . b d 2 2 2 2 2 d d d d d d c . 
+        . . b b 2 2 2 d d d d d b c . . 
+        . . . b b c c c c c c c c . . . 
+        `)
+})
 controller.player1.onButtonEvent(ControllerButton.B, ControllerButtonEvent.Pressed, function () {
     player1.setPosition(8, 10)
 })
-controller.player2.onButtonEvent(ControllerButton.B, ControllerButtonEvent.Pressed, function () {
-    player2.setPosition(8, 54)
+controller.player4.onButtonEvent(ControllerButton.Right, ControllerButtonEvent.Pressed, function () {
+    player4.setImage(img`
+        . . . . . . . . . . b 9 b . . . 
+        . . . . . . . . . b 9 b . . . . 
+        . . . . . . . . . b c . . . . . 
+        . . . . . . b b b b b b . . . . 
+        . . . . . b b 9 9 9 9 9 b . . . 
+        . . . . b b 9 6 1 f 9 9 6 f . . 
+        . . . . b 9 9 1 f f 9 6 4 c . . 
+        . . . . b 9 9 6 f b 6 6 4 4 . . 
+        b 6 6 6 b b 6 9 9 9 4 4 4 4 4 b 
+        b b 6 9 9 9 b 9 9 4 4 4 4 4 b . 
+        b 6 c 9 9 9 9 6 9 9 9 9 9 b . . 
+        c 6 6 c 6 9 9 b 9 9 9 9 9 9 b . 
+        c b 6 6 c c b 9 9 9 9 9 9 9 b . 
+        . c 6 6 6 6 6 6 9 9 9 9 9 6 b . 
+        . . c b 6 6 6 6 6 9 9 9 b b . . 
+        . . . c c c c c c c c b b . . . 
+        `)
 })
-let mySprite: Sprite = null
+controller.player2.onButtonEvent(ControllerButton.Right, ControllerButtonEvent.Pressed, function () {
+    player2.setImage(img`
+        . . . . . . . . . . b 5 b . . . 
+        . . . . . . . . . b 5 b . . . . 
+        . . . . . . . . . b c . . . . . 
+        . . . . . . b b b b b b . . . . 
+        . . . . . b b 5 5 5 5 5 b . . . 
+        . . . . b b 5 d 1 f 5 5 d f . . 
+        . . . . b 5 5 1 f f 5 d 4 c . . 
+        . . . . b 5 5 d f b d d 4 4 . . 
+        b d d d b b d 5 5 5 4 4 4 4 4 b 
+        b b d 5 5 5 b 5 5 4 4 4 4 4 b . 
+        b d c 5 5 5 5 d 5 5 5 5 5 b . . 
+        c d d c d 5 5 b 5 5 5 5 5 5 b . 
+        c b d d c c b 5 5 5 5 5 5 5 b . 
+        . c d d d d d d 5 5 5 5 5 d b . 
+        . . c b d d d d d 5 5 5 b b . . 
+        . . . c c c c c c c c b b . . . 
+        `)
+})
+controller.player2.onButtonEvent(ControllerButton.B, ControllerButtonEvent.Pressed, function () {
+    player2.setPosition(40, 10)
+})
+let player4: Sprite = null
+let player3: Sprite = null
 let player2: Sprite = null
 let player1: Sprite = null
 scene.setBackgroundImage(img`
@@ -178,11 +353,11 @@ player2 = sprites.create(img`
     . c d d d d d d 5 5 5 5 5 d b . 
     . . c b d d d d d 5 5 5 b b . . 
     . . . c c c c c c c c b b . . . 
-    `, SpriteKind.a)
-player2.setPosition(8, 10)
-controller.player1.moveSprite(player2, 100, 0)
+    `, SpriteKind.b)
+player2.setPosition(40, 10)
+controller.player2.moveSprite(player2, 100, 0)
 player2.ay = 500
-let player3 = sprites.create(img`
+player3 = sprites.create(img`
     . . . . . . . . . . b 7 b . . . 
     . . . . . . . . . b 7 b . . . . 
     . . . . . . . . . b c . . . . . 
@@ -199,11 +374,32 @@ let player3 = sprites.create(img`
     . c 6 6 6 6 6 6 7 7 7 7 7 6 b . 
     . . c b 6 6 6 6 6 7 7 7 b b . . 
     . . . c c c c c c c c b b . . . 
-    `, SpriteKind.b)
+    `, SpriteKind.c)
 player3.ay = 500
 player3.setPosition(8, 54)
-controller.player2.moveSprite(player3, 100, 0)
+controller.player3.moveSprite(player3, 100, 0)
+player4 = sprites.create(img`
+    . . . . . . . . . . b 9 b . . . 
+    . . . . . . . . . b 9 b . . . . 
+    . . . . . . . . . b c . . . . . 
+    . . . . . . b b b b b b . . . . 
+    . . . . . b b 9 9 9 9 9 b . . . 
+    . . . . b b 9 6 1 f 9 9 6 f . . 
+    . . . . b 9 9 1 f f 9 6 4 c . . 
+    . . . . b 9 9 6 f b 6 6 4 4 . . 
+    b 6 6 6 b b 6 9 9 9 4 4 4 4 4 b 
+    b b 6 9 9 9 b 9 9 4 4 4 4 4 b . 
+    b 6 c 9 9 9 9 6 9 9 9 9 9 b . . 
+    c 6 6 c 6 9 9 b 9 9 9 9 9 9 b . 
+    c b 6 6 c c b 9 9 9 9 9 9 9 b . 
+    . c 6 6 6 6 6 6 9 9 9 9 9 6 b . 
+    . . c b 6 6 6 6 6 9 9 9 b b . . 
+    . . . c c c c c c c c b b . . . 
+    `, SpriteKind.d)
+player4.ay = 500
+player4.setPosition(40, 54)
+controller.player4.moveSprite(player4, 100, 0)
 splitScreen.cameraFollowSprite(splitScreen.Camera.Camera1, player1)
 splitScreen.cameraFollowSprite(splitScreen.Camera.Camera2, player2)
 splitScreen.cameraFollowSprite(splitScreen.Camera.Camera3, player3)
-splitScreen.cameraFollowSprite(splitScreen.Camera.Camera4, mySprite)
+splitScreen.cameraFollowSprite(splitScreen.Camera.Camera4, player4)
